31212573738dcc21f68af0270e48fa78f87e327d
rahmasari16 <ragmasari.added@gmail.com>
2020-04-25 22:29:46 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,25 @@ public class InputOutput{
  public static void main(String [] args){
   Scanner scanner =  new Scanner(System.in);
   String nama;
+  int jumlahSks;
+  double IPK;
+
+  System.out.println("*** INI ADALAH PROGRAM PBO1 ***");
+
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
+  System.out.println("Hai, "+nama);
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = scanner.nextInt();
+
+  System.out.print("Masukkan IPK sementara :");
+  IPK = scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.println("Total Nilai = "+jumlah);
  
+  System.out.println("BERHASIL..!!");
+
  }
 }
